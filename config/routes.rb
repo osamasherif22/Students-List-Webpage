f16a38f31d0ced648d015c49732d351dbd1428ec
Osama Sherif <osama.sh.rabia19@gmail.com>
2022-09-20 12:42:04 +0200

    intial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :students
+  #get 'home/index'
+  #root 'home#index'
+  get 'home/about'
+  root 'students#index'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
